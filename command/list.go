707540af8e82caadab44f4c69e9c2568dc18806c
Kimura Masayuki <masa0x80@gmail.com>
2017-07-05 23:45:03 +0900

    [#1] Implement `list` command

---
+++ command/list.go
@@ -0,0 +1,59 @@
+package command
+
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"strings"
+)
+
+// ListCommand is the struct for the list command.
+type ListCommand struct{}
+
+// Run runs the command.
+// The args are the arguments after the command name.
+func (c *ListCommand) Run(args []string) int {
+	var dirName string
+	if len(args) > 0 {
+		dirName = args[0]
+	} else {
+		dirName = "."
+	}
+	dirPath, _ := filepath.Abs(dirName)
+	if !isDir(dirPath) {
+		fmt.Fprintf(os.Stderr, "ERROR: Invalid argument\n")
+		return 1
+	}
+	secretsRoot, err := secretsRoot()
+	if err != nil {
+		fmt.Fprintf(os.Stderr, "ERROR: Failed to execute: %s\n", err.Error())
+		return 1
+	}
+	remoteDir := filepath.Join(secretsRoot, dirPath)
+	for _, path := range traverseFiles(remoteDir) {
+		relPath, _ := filepath.Rel(remoteDir, path)
+		fmt.Fprintf(os.Stdout, "%s\n", relPath)
+	}
+
+	return 0
+}
+
+// Synopsis is the short message shown in the 'secman help' output.
+func (c *ListCommand) Synopsis() string {
+	return "List secret files"
+}
+
+// Help is the long description shown in the 'secman help version' output.
+func (c *ListCommand) Help() string {
+	helpText := `
+NAME:
+		secman list - list secret files
+
+USAGE:
+		secman list
+
+DESCRIPTION:
+		List secret files under secman control.
+`
+	return strings.TrimSpace(helpText)
+}
--- command/util.go
+++ command/util.go
@@ -1,6 +1,7 @@
 package command
 
 import (
+	"io/ioutil"
 	"os"
 	"os/user"
 	"path/filepath"
@@ -13,6 +14,15 @@ func isFile(file string) bool {
 	}
 	isSymlink := fi.Mode()&os.ModeSymlink != 0
 	return !isSymlink && fi.Mode().IsRegular()
+}
+
+func isDir(file string) bool {
+	fi, err := os.Lstat(file)
+	if os.IsNotExist(err) {
+		return false
+	}
+	isSymlink := fi.Mode()&os.ModeSymlink != 0
+	return !isSymlink && fi.Mode().IsDir()
 }
 
 func isSymlink(file string) bool {
@@ -30,4 +40,21 @@ func secretsRoot() (string, error) {
 	}
 	homeDir := u.HomeDir
 	return filepath.Join(homeDir, ".secrets"), nil
+}
+
+func traverseFiles(dir string) []string {
+	var paths []string
+
+	files, err := ioutil.ReadDir(dir)
+	if err == nil {
+		for _, file := range files {
+			if file.IsDir() {
+				paths = append(paths, traverseFiles(filepath.Join(dir, file.Name()))...)
+				continue
+			}
+			paths = append(paths, filepath.Join(dir, file.Name()))
+		}
+	}
+
+	return paths
 }
