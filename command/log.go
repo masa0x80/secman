86e3c4cabd9243e4a11f94ecc0c1ba7d9a2b4f41
Kimura Masayuki <masa0x80@gmail.com>
2017-07-09 19:12:17 +0900

    [#5] Add log functions

---
+++ command/log.go
@@ -0,0 +1,43 @@
+package command
+
+import (
+	"os"
+
+	"github.com/motemen/go-colorine"
+)
+
+var logger = colorine.NewLogger(
+	colorine.Prefixes{
+		"Create":  colorine.Notice,
+		"Delete":  colorine.Notice,
+		"Symlink": colorine.Notice,
+		"Exist":   colorine.Warn,
+
+		"Error": colorine.Warn,
+		"Skip":  colorine.Info,
+	},
+	colorine.Info,
+)
+
+// Log prints messages
+func Log(prefix, message string) {
+	logger.Log(prefix, message)
+}
+
+// ErrorIf prints messages, if error occured
+func ErrorIf(err error) bool {
+	if err != nil {
+		Log("Error", err.Error())
+		return true
+	}
+
+	return false
+}
+
+// DieIf prints messages and exit, if error occured
+func DieIf(err error) {
+	if err != nil {
+		Log("Error", err.Error())
+		os.Exit(1)
+	}
+}
